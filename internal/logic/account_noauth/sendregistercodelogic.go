1c9bd3981afcde90c919272a883e31d6d6c26630
yuan-shuo <369599207@qq.com>
2026-03-25 21:35:23 +0800

    refactor(account_noauth): 将常量定义移动到独立的const.go文件
    将sendregistercodelogic.go中的常量定义提取到单独的const.go文件，提高代码组织性和可维护性

--- internal/logic/account_noauth/sendregistercodelogic.go
+++ internal/logic/account_noauth/sendregistercodelogic.go
@@ -19,13 +19,6 @@ import (
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
-const (
-	verifyKey               string = "verify" // 包级验证redis键词缀
-	limitKey                string = "limit"  // 包级限流redis键词缀
-	redisValueCodeFieldName string = "code"   // redis hash 验证码值的键名
-	redisValueUsedFieldName string = "used"   // redis hash 是否使用过值的键名 "0": 未使用过
-)
-
 type SendRegisterCodeLogic struct {
 	logx.Logger
 	ctx    context.Context
