629e74200a2c6025b3fdb348315b2f310b0862e8
yuan-shuo <369599207@qq.com>
2026-03-29 11:44:26 +0800

    refactor: 重构验证码校验相关逻辑，将部分实例函数调整为通用函数

--- internal/logic/account_noauth/resetpasswordlogic.go
+++ internal/logic/account_noauth/resetpasswordlogic.go
@@ -27,7 +27,27 @@ func NewResetPasswordLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Res
 }
 
 func (l *ResetPasswordLogic) ResetPassword(req *types.ResetPasswordReq) (resp *types.ResetPasswordResp, err error) {
-	// todo: add your logic here and delete this line
+	// 所需验证码类型 - 重置密码
+	codeType := l.svcCtx.Config.VerifyCodeConfig.Type.ResetPassword
+
+	// 检查验证码是否属于对应邮箱以及是否正确
+	if err := verifyEmailAndCodeInRedis(l.ctx, l.svcCtx, req.Email, req.Code, codeType); err != nil {
+		return nil, err
+	}
+
+	// 密码加密
+	newHashedPassword, err := hashPassword(req.Email, req.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	// 重置用户密码
+	if err := resetUserPassword(l.ctx, l.svcCtx, req.Email, newHashedPassword); err != nil {
+		return nil, err
+	}
+
+	// 标记已被使用
+	markCodeAsUsed(l.ctx, l.svcCtx, req.Email, codeType)
 
 	return
 }
